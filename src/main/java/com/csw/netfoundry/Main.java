4a9dc15013fb8e413b0deae3ceea6c135c184aa3
cwjuggernaut <chris.walker@netfoundry.io>
2021-09-09 11:58:40 -0400

    updating yml, pom, main

--- src/main/java/com/csw/netfoundry/Main.java
+++ src/main/java/com/csw/netfoundry/Main.java
@@ -3,6 +3,7 @@ import org.openziti.Ziti;
 import org.openziti.ZitiAddress;
 import org.openziti.ZitiContext;
 
+import java.io.IOException;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 
@@ -17,15 +18,18 @@ public class Main {
 
     public static void connectAsServer() {
         System.out.println("INFO :: Connecting Java SDK as Server");
-        ZitiContext context = Ziti.newContext(ID_FILE, "".toCharArray());
-//        AsynchronousServerSocketChannel asC = context.openServer();
-        AsynchronousSocketChannel asC = context.open();
-        asC.bind(ZitiAddress.Bind());
+        try {
+            ZitiContext context = Ziti.newContext(ID_FILE, "".toCharArray());
+            AsynchronousServerSocketChannel asC = context.openServer();
+            asC.bind(new ZitiAddress.Bind("service_name"));
+        } catch (IOException ioe) {
+            System.out.println("ERROR => Caught Exception: " + ioe);
+        }
 
     }
 
     public static void main(String[] args){
-//        connectAsClient();
+        String service = args[0];
         connectAsServer();
     }
 }
